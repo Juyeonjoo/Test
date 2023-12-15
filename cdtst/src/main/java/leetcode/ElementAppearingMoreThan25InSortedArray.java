75561ff5d5f17382abd4f5fd8106d5fec8790031
Juyeonjoo <yjjjoo0331@gmail.com>
2023-12-15 17:43:04 +0900

    ElementAppearingMoreThan25InSortedArray commit

---
+++ cdtst/src/main/java/leetcode/ElementAppearingMoreThan25InSortedArray.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ElementAppearingMoreThan25InSortedArray {
+
+    public int findSpecialInteger(int[] arr) {
+        /*
+        * Example 1:
+        Input: arr = [1,2,2,6,6,6,6,7,10]
+        Output: 6
+        *
+        Example 2:
+        Input: arr = [1,1]
+        Output: 1
+        * */
+        int result =0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int sum =0;
+            for(int j = 0; j<arr.length; j++){
+
+               if(arr[i] == arr[j]){
+                   sum +=1;
+               };
+            }
+            try{
+                map.put(arr[i], sum);
+            }catch (Exception e){
+                continue;
+            }
+        }//outer for
+        Integer max = Collections.max(map.values());
+        if(max>arr.length/4){
+            for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+                if(entry.getValue()==max){
+                    result = entry.getKey();
+               }
+        }}
+        System.out.println("max = " + max);
+        System.out.println("result = " + result);
+        return result;
+    }//main
+}//class
---
+++ cdtst/src/test/java/leetcode/ElementAppearingMoreThan25InSortedArrayTest.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+class ElementAppearingMoreThan25InSortedArrayTest {
+
+
+    @Test
+    public void findSpecialInteger(){
+        //given
+        ElementAppearingMoreThan25InSortedArray elementAppearingMoreThan25InSortedArray = new ElementAppearingMoreThan25InSortedArray();
+        int[] arr = {1,1};
+
+        elementAppearingMoreThan25InSortedArray.findSpecialInteger(arr);
+
+        //when
+        //then
+    }
+
+}
