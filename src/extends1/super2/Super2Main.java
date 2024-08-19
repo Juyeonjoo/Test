374175d521e9f7236d6f654811b271dcc7d0a36c
Juyeonjoo <yjjjoo0331@gmail.com>
2024-08-19 19:19:18 +0900

    다형성 다운캐스팅

--- src/extends1/super2/Super2Main.java
+++ src/extends1/super2/Super2Main.java
@@ -2,6 +2,9 @@ package extends1.super2;
 
 public class Super2Main {
     public static void main(String[] args) {
-        ClassC classC = new ClassC();
+        int a = 10;
+        System.out.println("a++ = " + a++);
+        System.out.println("a = " + a);
+
     } 
 }
